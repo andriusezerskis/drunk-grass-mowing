0831ef46981f32c71478332e94ab6b0984f78bd5
Cobalt1140 <hubert.twarowski@outlook.com>
2025-11-22 19:19:54 +0100

    muted music

--- src/view/mainWindow.py
+++ src/view/mainWindow.py
@@ -78,7 +78,7 @@ class Window(QMainWindow):
         self.music.setSource(QUrl.fromLocalFile("sound_effects/music.wav"))
         self.music.setLoops(-1)
         self.music.setAudioOutput(self.audioOutput)
-        self.audioOutput.setVolume(0.3)
+        self.audioOutput.setVolume(0)
         self.music.play()
        
         self.verticalLayout.addWidget(self.animationLabel, alignment=Qt.AlignmentFlag.AlignCenter)
